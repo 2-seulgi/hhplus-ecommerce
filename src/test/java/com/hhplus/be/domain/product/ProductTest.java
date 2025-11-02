6c1e1ecc09786c6749cde0bd590b07b2717ecac1
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2025-11-03 01:55:19 +0900

    test: Product 도메인 단위 테스트 작성
    - 재고 차감/복원 기능 테스트
    - 재고 부족 시 예외 처리 테스트
    - 유효성 검증 테스트 (양수 체크)

---
+++ src/test/java/com/hhplus/be/domain/product/ProductTest.java
@@ -0,0 +1,84 @@
+package com.hhplus.be.domain.product;
+
+import com.hhplus.be.common.exception.BusinessException;
+import com.hhplus.be.common.exception.InvalidInputException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ProductTest {
+
+    @Test
+    @DisplayName("재고를 차감할 수 있다")
+    void decreaseStock() {
+        // given
+        Product product = Product.create("맥북 PRO", "M3", 3000000, 10);
+
+        // when
+        product.decreaseStock(3);
+
+        // then
+        assertThat(product.getStock()).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("재고가 부족하면 차감할 수 없다")
+    void cannotDecreaseStockWhenOutOfStock() {
+        // given
+        Product product = Product.create("맥북 PRO", "M3", 3000000, 10);
+
+        // when & then
+        assertThatThrownBy(() -> product.decreaseStock(15))
+            .isInstanceOf(BusinessException.class)
+            .hasMessageContaining("재고가 부족합니다");
+    }
+
+    @Test
+    @DisplayName("재고 차감 수량은 양수여야 한다")
+    void  decreaseQuantityMustBePositive(){
+        // given
+        Product product = Product.create("맥북 PRO", "M3", 3000000, 10);
+
+        // when & then
+        assertThatThrownBy(() -> product.decreaseStock(0))
+            .isInstanceOf(InvalidInputException.class)
+            .hasMessageContaining("차감 수량은 양수여야 합니다");
+
+        assertThatThrownBy(() -> product.decreaseStock(-5))
+            .isInstanceOf(InvalidInputException.class)
+            .hasMessageContaining("차감 수량은 양수여야 합니다");
+    }
+
+    @Test
+    @DisplayName("재고를 복원할 수 있다")
+    void increaseStock() {
+        // given
+        Product product = Product.create("맥북 PRO", "M3", 3000000, 10);
+
+        // when
+        product.increaseStock(5);
+
+        // then
+        assertThat(product.getStock()).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("재고 복원 수량은 양수여야 한다")
+    void increaseQuantityMustBePositive(){
+        // given
+        Product product = Product.create("맥북 PRO", "M3", 3000000, 10);
+
+        // when & then
+        assertThatThrownBy(() -> product.increaseStock(0))
+            .isInstanceOf(InvalidInputException.class)
+            .hasMessageContaining("복원 수량은 양수여야 합니다");
+
+        assertThatThrownBy(() -> product.increaseStock(-5))
+            .isInstanceOf(InvalidInputException.class)
+            .hasMessageContaining("복원 수량은 양수여야 합니다");
+    }
+
+
+}
